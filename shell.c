110376dfdaea780e4746d5e3b6427761c83a229d
Halimatou Bah <halimatou.bah@etu.u-pec.fr>
2020-03-29 22:14:54 +0200

    Code Shell reorganisé(bon) jusqu'au simple_cmd sans reaction au CTRL D mais à ENTREE plutot

--- shell.c
+++ shell.c
@@ -1,214 +1,205 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <error.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <error.h>
 #include <sys/wait.h>
 
-void affiche_cmd(char* argv[]) {
-  for (int i = 0; i<100 && argv[i]!=NULL; i++) {
-    write(STDOUT_FILENO, argv[i], strlen(argv[i]));
-    write(1, " ", 1);
-  }
-  write(1,"\n",1);
-}
+#define BUFSIZE 1024
+#define MAX_ARG 100
 
-char * sub_string (char * s, int i){
-  char* cont = (char*) malloc(sizeof(char)*(i+1));
-
-  for(unsigned int j=0; j<strlen(s) && j<(unsigned int)i ;j++){
-    cont[j]=s[j];
-  }
-  cont[i]='\0';
-  return cont;
-}
-
-//Retourne l'indice de la premiere occurence du caractere 'c'
-//dans la chaîne de caractère s
-int find_char(char* s, char c){
-  int len = strlen(s);
-  int i = 0;
-  for (i = 0; i < len; i++){
-    if(s[i] == c) return i;
-  }
-  return -1;
-}
-
-//Parseur qui découpe la chaine de caractere s
-int parse_line (char * s, char ** argv[]){
-
-  char* p1 = strpbrk(s,"#");
-  int len = p1 == NULL? 0 : strlen(p1);
-  int t = strlen(s) - len;
-
-  char *p = sub_string(s,t);
-  const char * sep = " ";
-  char* lu = strtok(p , sep);
-
-  int i = 0;
-  while(lu){
-    argv[i]= (char **) malloc(sizeof(char)*(1+ strlen(lu)));
-    (*argv)[i]=lu;
-    lu = strtok(NULL," ");
-    i++ ;
-  }
-  (*argv)[i] = NULL;
-  return i;
-}
-
-void test_parse(char *argv[]){
-  char * s= "commande -v toto mammamia #ecrt  tata fatou";
-
-  parse_line(s, (&argv) );
-  affiche_cmd (argv) ;
-
-  write(1, sub_string(s,4), strlen(sub_string(s,4)));
-}
-
-void simple_cmd (char * argv[]){
-  if (argv[0] == NULL) return;
-  pid_t p;
-  if(strcmp(argv[0],"exit")==0) exit(EXIT_SUCCESS);
-  else if (strcmp(argv[0],"cd")==0){
-    // int change;
-    if(argv[1]!=NULL){
-      write(1, argv[1], strlen(argv[1]));
-      write(1,"\n",1);
-      // change =
-      chdir(argv[1]);
-      //if (change == -1) write(STDERR_FILENO,"Directory changement not done",BUFSIZ);
-    }else {
-      char parse [BUFSIZ];
-      getcwd (parse, BUFSIZ) ;
-      unsigned int len = strlen (parse);
-      int count=0;
-      unsigned int i;
-      for (i = 0; i < len && count <3 ; i++) {
-        if(parse[i]=='/') count++ ;
-      }
-      write(1, sub_string(parse, i), strlen(sub_string(parse, i)));
-      // change =
-      chdir (sub_string(parse, i));
+//Fonction qui affiche tous les elements d'un tableau
+void affiche_cmd(char *argv[])
+{
+    for (int i = 0; i < 100 && argv[i] != NULL; i++)
+    {
+        write(STDOUT_FILENO, argv[i], strlen(argv[i]));
+        write(1, " ", 1);
     }
-  }else{
-    p = fork();
-    if(p == 0){
-      execvp(argv[0],argv);
-      wait(0);
+    write(1, "\n", 1);
+}
+
+//fonction qui recupere une partie d'une chaine de car jusqu'a l'indice i
+char *sub_string(const char *s, int i)
+{
+    char *cont = (char *)malloc(sizeof(char) * (i + 1));
+
+    for (unsigned int j = 0; j < strlen(s) && j < (unsigned int)i; j++)
+    {
+        cont[j] = s[j];
     }
-  }
+    cont[i] = '\0';
+    return cont;
 }
 
-void cmd_main(){
-  // char reader[BUFSIZ];
-  char * reader=(char*)malloc(sizeof(char *));
-  int lu;
+//Parse en fonction des espaces (question 5)
+int parse_line(char *s, char **argv[])
+{
 
-  char curdir[BUFSIZ];
-  getcwd(curdir, BUFSIZ);
-  write (1,curdir,strlen(curdir));
-  write(1,"$ ",2);
+    char *p1 = strpbrk(s, "#");
+    int len = p1 == NULL ? 0 : strlen(p1);
+    int t = strlen(s) - len;
 
-  char **holder ;
-  // char *holder[100];
+    char *p = sub_string(s, t);
+    const char *sep = " ";
+    char *lu = strtok(p, sep);
 
-  while( (lu=read(STDIN_FILENO,reader,BUFSIZ)) > 0){
-    getcwd(curdir, BUFSIZ);
-
-    write (1,curdir,strlen(curdir));
-    write(1,"$ ",2);
-    write(1,"\n",1);
-
-    parse_line(reader, &holder);
-    affiche_cmd (holder);
-    simple_cmd (holder);
-    // free(holder);
-  }
-
-}
-
-void script_main(char *argv[]){
-  if(argv[1]){
-    int fd = open(argv[1], O_RDONLY | O_NONBLOCK, 0773);
-    if(fd != -1){
-      write(1,"voyons ",6);
-      int lire;
-      char ** tab;
-      char buffer[BUFSIZ];
-      while ((lire=read(fd,buffer,BUFSIZ))>0){
-        write(1,"Rienderien",10);
-        parse_line(buffer, &tab);
-        affiche_cmd(tab);
-        simple_cmd(tab);
-        write(1,"wesh",4);
-      }
-      close(fd);
-    }else{
-       write(1,"script pas ouvert",17);
+    int i = 0;
+    while (lu)
+    {
+        argv[i] = (char **)malloc(sizeof(char) * (1 + strlen(lu)));
+        (*argv)[i] = lu;
+        lu = strtok(NULL, " ");
+        i++;
     }
-  }
+    (*argv)[i] = NULL;
+    return i;
 }
 
-int parse_line_redir (char* s, char **argv[], char **in,char **out){
+//Parseur qui ne prend pas de tableau mais en renvoi un apres avoir parsé une chaine
+char **parse_line_bis(const char *cmd)
+{
+    char **result = malloc(MAX_ARG * (sizeof(char *)));
 
-  parse_line(s,argv);
-  int i = 1;
-  int len=0;
-  while ((*argv)[i]){
-    if ( (find_char ((*argv)[i],'>') ) != -1 ){
-      in[len] = (char *) malloc(sizeof(char)*(1+strlen((*argv)[i-1])));
-      in[len] = (*argv)[i-1];
-      if((*argv)[i+1]){
-        out[len] = (char *) malloc(sizeof(char)*(1+strlen((*argv)[i+1])));
-        out[len] = (*argv)[i+1];
-      }else out[len] = NULL;
-      len++;
+    char *p1 = strpbrk(cmd, "#");
+    int len = p1 == NULL ? 0 : strlen(p1);
+    int t = strlen(cmd) - len;
+
+    char *p = sub_string(cmd, t);
+    char *lu = strtok(p, " ");
+
+    int i = 0;
+    while (lu)
+    {
+        result[i] = (char *)malloc(sizeof(char) * (1 + strlen(lu)));
+        strcpy(result[i], lu);
+        lu = strtok(NULL, " ");
+        i++;
     }
-    if ( (find_char((*argv)[i],'<') ) != -1 ){
-      out[len] = (char *) malloc(sizeof(char)*(1+strlen((*argv)[i-1])));
-      out[len] = (*argv)[i-1];
-      if((*argv)[i+1]){
-        in[len] = (char *) malloc(sizeof(char)*(1+strlen((*argv)[i+1])));
-        in[len] = (*argv)[i+1];
-      }else in[len] = NULL;
-      len++;
+    result[i] = NULL;
+
+    return result;
+}
+
+// une procedure qui libere tous les mallocs char**
+void free_2D(char **array)
+{
+    int i = 0;
+    while (array[i])
+        free(array[i++]);
+}
+
+/*Fonction qui crée un processus fils qui exécute une cmd
+void executor(char *command[])
+{
+    pid_t pid = fork();
+
+    if (pid > 0)
+    {
+        wait(NULL);
     }
-    i++;
-  }
-  return 0;
+    else if (!pid)
+    {
+        execvp(command[0], command);
+        write(STDERR_FILENO, "Erreur dans execvp\n", 20);
+    }
+    else
+    {
+        // error
+    }
+}
+*/
+
+//fonction qui nettoie un buffer
+void cleaner(char *buffer)
+{
+    int i;
+    for (i = 0; i < BUFSIZE; i++)
+        buffer[i] = '\0';
 }
 
-//oui? Cool
+void simple_cmd(char *argv[]);
 
-int main(int argc, char *argv[]) {
-  test_parse(argv);
-  cmd_main();
-  script_main(argv);
-  write (1,argv[argc],0);
-  // char ** tab = (char **)malloc(sizeof(char*));
-  // char ** tab[100] ;
-  // char ** out = (char **) malloc(sizeof(char*));
-  // char ** in = (char **) malloc(sizeof(char*));
-  // parse_line_redir ("test < hali momo voir > alors",tab,in,out);
+void cmd_executor()
+{
+    int tmp;
+    char INPUT[BUFSIZE];
+    char **result;
 
-  // write(1,"\n",1);
-  // write(1,"le contenu de out: ",19);
-  // affiche_cmd(out);
-  // write(1,"\n",1);
-  // write(1,"contenu de in: ",15);
-  // affiche_cmd(in);
+    while (1)
+    {
+        cleaner(INPUT);
 
-  // free (tab);
-  // free (out);
-  // free (in);
+        tmp = read(STDIN_FILENO, INPUT, BUFSIZE);
 
-  return 0;
+        if (tmp == -1)
+        {
+            write(STDERR_FILENO, "non valid\n", 10);
+            exit(-1);
+        }
+
+        INPUT[tmp - 1] = '\0';
+
+        result = parse_line_bis(INPUT);
+
+        simple_cmd(result);
+
+        // executor(result);
+        free_2D(result);
+    }
 }
 
-//un fork pour toutes les fonc _cmd
+void simple_cmd(char *argv[])
+{
+    if (argv[0] == NULL)
+        return;
+    pid_t p;
+    if (strcmp(argv[0], "exit") == 0)
+        exit(EXIT_SUCCESS);
+    else if (strcmp(argv[0], "cd") == 0)
+    {
+        if (argv[1] != NULL)
+        {
+            write(1, argv[1], strlen(argv[1]));
+            write(1, "\n", 1);
+            chdir(argv[1]);
+        }
+        else
+        {
+            char parse[BUFSIZE];
+            getcwd(parse, BUFSIZE);
+            unsigned int len = strlen(parse);
+            int count = 0;
+            unsigned int i;
+            for (i = 0; i < len && count < 3; i++)
+            {
+                if (parse[i] == '/')
+                    count++;
+            }
+            write(1, sub_string(parse, i), strlen(sub_string(parse, i)));
+            chdir(sub_string(parse, i));
+        }
+    }
+    else
+    {
+        p = fork();
+        if (p == 0)
+        {
+            execvp(argv[0], argv);
+            wait(0);
+        }
+    }
+}
 
-//Note à moi même, me debarrasser de tous mes printf, les
-//remplacer par des read et write avant de rendre ma version finale sur le git
+int main(int argc, char *argv[])
+{
+    write(1, argv[argc], 0);
+    // affiche_cmd(parse_line_bis("voyons cette commande # apres parse"));
+
+    //l'appel de cmd_executor dans le main contient la question 6
+    //avec en plus l'exécution des commandes une fois que l'utilisateur
+    //tape sur entrée
+    cmd_executor();
+    return 0;
+}
